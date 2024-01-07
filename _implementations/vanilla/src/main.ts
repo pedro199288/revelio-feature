824e91aa1ecb6a00586e2f1e304dc726fa22be15
Pedro Monteagudo <pedromonteagudo_@hotmail.com>
2024-01-07 11:21:21 +0100

    only execute CI typecheck on library code

--- _implementations/vanilla/src/main.ts
+++ _implementations/vanilla/src/main.ts
@@ -2,7 +2,7 @@ import './style.css';
 import typescriptLogo from './typescript.svg';
 import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.ts';
-import { Revelio } from 'bun-library-starter';
+import { Revelio } from 'revelio-feature';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
