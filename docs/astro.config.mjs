1c3c8733065a5f93d049cea266ab7c767cdd7539
Pedro Monteagudo <pedromonteagudo_@hotmail.com>
2024-01-26 12:53:05 +0100

    added tailwindcss, update colors and added images

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,18 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'Revelio Feature',
+      favicon: './src/assets/revelio-feature-favicon.png',
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            href: './src/assets/revelio-feature-favicon.ico',
+            sizes: '32x32',
+          },
+        },
+      ],
+      logo: { src: './src/assets/revelio-feature-favicon.png' },
       social: {
         github: 'https://github.com/pedro199288/revelio-feature',
       },
@@ -17,14 +30,21 @@ export default defineConfig({
           label: 'Guides',
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
+            {
+              label: 'Example Guide',
+              link: '/guides/example/',
+            },
           ],
         },
         {
           label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          autogenerate: {
+            directory: 'reference',
+          },
         },
       ],
+      customCss: ['./src/tailwind.css'],
     }),
+    tailwind(),
   ],
 });
---
+++ docs/tailwind.config.mjs
@@ -0,0 +1,19 @@
+import starlightPlugin from '@astrojs/starlight-tailwind';
+import colors from 'tailwindcss/colors';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        // Your preferred accent color. Indigo is closest to Starlight’s defaults.
+        accent: colors.amber,
+        // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
+        gray: colors.stone,
+      },
+    },
+  },
+
+  plugins: [starlightPlugin()],
+};
