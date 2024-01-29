ef0e4964e9c333f22b3581139065e0e14335e072
Pedro Monteagudo <pedromonteagudo_@hotmail.com>
2024-01-29 13:50:02 +0100

    fix favicon

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -12,18 +12,18 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'Revelio Feature',
-      favicon: './favicon.png',
+      favicon: './public/favicon.png',
       head: [
         {
           tag: 'link',
           attrs: {
             rel: 'icon',
-            href: './favicon.ico',
+            href: './public/favicon.ico',
             sizes: '32x32',
           },
         },
       ],
-      logo: { src: './favicon.png' },
+      logo: { src: './public/favicon.png' },
       social: {
         github: 'https://github.com/pedro199288/revelio-feature',
       },
