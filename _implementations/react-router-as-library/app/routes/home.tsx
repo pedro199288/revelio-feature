e290cd3040c53daa694d7ba61e4c0b7fc3eff5a9
Pedro Monteagudo <pmonteagudojimenez@gmail.com>
2024-12-09 18:33:37 +0100

    add react example

---
+++ _implementations/react-router-as-library/app/routes/home.tsx
@@ -0,0 +1,14 @@
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <div className="text-3xl font-bold">
+    hello world
+  </div>
+}
